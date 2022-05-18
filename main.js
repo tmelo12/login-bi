5bfc5a72a4bbc31e00b3b844be4f6e7f069d6afd
Thiago Braga de Melo <103153661+tmelo12@users.noreply.github.com>
2022-05-18 10:30:43 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+function index(req,res){
+   res.render("login")
+}
+
+function login(req, res){
+    if(req.body.login === 'governo' && req.body.senha === '1705'){
+        req.session.user = req.body.login;
+        res.redirect("bi");
+    }
+    else if(req.body.login === 'dc-seas' && req.body.senha === '1805'){
+        req.session.user = req.body.login;
+        res.redirect("bi");
+    }
+    else if(req.body.login === 'visitante' && req.body.senha === '1905'){
+        req.session.user = req.body.login;
+        res.redirect("bi");
+    }
+    else{
+        req.session.user = 'undefined';
+        return res.render("login", {
+            message: "Sua conta ou senha está incorreta."
+        });
+    }
+}
+
+function bi(req,res){
+    if( req.session.user==='governo' ){
+        res.render('bi');
+    }
+    else if( req.session.user==='dc-seas' ){
+        res.render('bi_seas')
+    }
+    else if( req.session.user==='visitante' ){
+        res.render('bi_vis')
+    }
+    else{
+        res.redirect("/");
+    }
+}
+
+function sair(req, res){
+    req.session.user = 'undefined';
+    res.redirect("/");
+}
+
+module.exports = {index, bi, login, sair}
